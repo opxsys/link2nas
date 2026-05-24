d1a7a5750d5d9188165c54f949be565308f005eb
Leang <leanima@yahoo.fr>
2026-05-24 12:30:41 +0200

    refactor: split app controller events

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,18 +1,12 @@
-import { clearToken } from "./core/session.js";
-import { state } from "./state.js";
-import { logout } from "./api.js";
 import { loadSettings } from "./controllers/settings-controller.js";
 import { bindGlobalNavigationEvents } from "./controllers/app/global-navigation-events.js";
 import { bindThemeEvents } from "./controllers/app/theme-events.js";
+import { bindSessionEvents } from "./controllers/app/session-events.js";
 import { bindJobsEvents } from "./controllers/jobs-controller.js";
 import { bindSettingsEvents } from "./events/settings-events.js";
 import { bindAdminEvents } from "./events/admin-events.js";
 import { bindAnnouncementsPageEvents } from "./controllers/announcements-controller.js";
-import {
-  closeNavDrawer,
-  setActivePage,
-  updateAuthVisibility,
-} from "./controllers/navigation-controller.js";
+import { setActivePage } from "./controllers/navigation-controller.js";
 import { run } from "./bootstrap.js";
 
 let globalEventsBound = false;
@@ -41,18 +35,7 @@ function bindGlobalEvents() {
   bindSettingsEvents();
   bindAdminEvents();
 
-  document.getElementById("logout-btn")?.addEventListener("click", async () => {
-    closeNavDrawer();
-
-    try {
-      await logout();
-    } catch {}
-
-    state.currentUser = null;
-    updateAuthVisibility();
-    clearToken();
-    location.reload();
-  });
+  bindSessionEvents();
 }
 
 run(bindGlobalEvents).catch((error) => {
