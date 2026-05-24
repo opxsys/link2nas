ad9c03e4d827a37c8f69d6df5389bdead72bdb02
Leang <leanima@yahoo.fr>
2026-05-24 03:29:29 +0200

    refactor: split modal helpers

---
+++ frontend/js/ui/modals/index.js
@@ -0,0 +1,4 @@
+export { escapeForModalHtml, formatDateForModal, toAbsoluteAppUrl } from "./utils.js";
+export { showConfirmModal } from "./confirm.js";
+export { showLinkModal } from "./link.js";
+export { showSecretModal } from "./secret.js";
